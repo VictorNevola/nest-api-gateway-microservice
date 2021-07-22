fb0e004ca231797e03f096aa1e66162d899f6325
Victor Nevola <victornevolapn@gmail.com>
2021-07-21 23:52:54 -0300

    Feat: init RabbitMQ

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,31 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Logger, Post, UsePipes, ValidationPipe } from '@nestjs/common';
+import { ClientProxy, ClientProxyFactory, Transport } from '@nestjs/microservices';
 import { AppService } from './app.service';
+import { CriarCategoriaDto } from './dtos/criar-categoria-dto';
 
-@Controller()
+@Controller('api/v1')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  private logger = new Logger(AppController.name);
+
+  private clientAdminBackEnd: ClientProxy;
+
+  constructor() {
+    this.clientAdminBackEnd = ClientProxyFactory.create({
+      transport: Transport.RMQ,
+      options: {
+        urls: [`amqp://user:${process.env.USERRABBIT}@${process.env.SERVERAWSEC2}/smartranking`],
+        queue: 'admin-backend'
+      }
+    })
   }
+
+  @Post('categorias')
+  @UsePipes(ValidationPipe)
+  criarCategoria(
+    @Body() criarCategoriaDto: CriarCategoriaDto
+  ){
+    return this.clientAdminBackEnd.emit('criar-categoria', criarCategoriaDto);
+  }
+
 }

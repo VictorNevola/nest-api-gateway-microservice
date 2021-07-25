3f3b3fd36e8fcd30d11460a26dec0d321acbf0bf
Victor Nevola <victornevolapn@gmail.com>
2021-07-25 20:30:57 -0300

    Feat: created controller to att categories

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Get, Logger, Post, Query, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Get, Logger, Param, Post, Put, Query, UsePipes, ValidationPipe } from '@nestjs/common';
 import { ClientProxy, ClientProxyFactory, Transport } from '@nestjs/microservices';
 import { Observable } from 'rxjs';
 import { AppService } from './app.service';
+import { AtualizarCategoriaDto } from './dtos/atualizar-categoria-dto';
 import { CriarCategoriaDto } from './dtos/criar-categoria-dto';
 
 @Controller('api/v1')
@@ -25,15 +26,24 @@ export class AppController {
   @UsePipes(ValidationPipe)
   criarCategoria(
     @Body() criarCategoriaDto: CriarCategoriaDto
-  ){
+  ) {
     this.clientAdminBackEnd.emit('criar-categoria', criarCategoriaDto);
   }
 
   @Get('categorias')
   consultarCategorias(
     @Query('idCategoria') _id: string
-  ): Observable<any>{
+  ): Observable<any> {
     return this.clientAdminBackEnd.send('consultar-categorias', _id ? _id : '')
+  }
+
+  @Put('/categorias/:idCategoria')
+  @UsePipes(ValidationPipe)
+  async atualizarCategoria(
+    @Body() atualizarCategoriaDto: AtualizarCategoriaDto,
+    @Param('idCategoria') _id: string,
+  ) {
+    this.clientAdminBackEnd.emit('atualizar-categoria', { idCategoria: _id, categoriaAtualizada: atualizarCategoriaDto })
   }
 
 }

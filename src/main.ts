ef0c6ac516b94c8a22c180de3129860fa29ebd1a
Victor Nevola <victornevolapn@gmail.com>
2021-07-25 19:43:22 -0300

    Feat: created interceptors global

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,15 @@ import * as momentTimeZone from 'moment-timezone';
 
 import { AppModule } from './app.module';
 import { AllExceptionsFilter } from './filters/http-exception.filter';
+import { LoggingInterceptor } from './interceptors/logging.interceptor';
+import { TimeOutInterceptor }  from './interceptors/timeout.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalInterceptors(new LoggingInterceptor, new TimeOutInterceptor());
   app.useGlobalFilters(new AllExceptionsFilter());
 
-  Date.prototype.toJSON = function(): any {
+  Date.prototype.toJSON = function (): any {
     return momentTimeZone(this).tz('America/Sao_Paulo').format('YYYY-MM-DD HH:mm:ss.SSS');
   }
 
